ef74a8acfe443d6e4e3786e97afbe30a4027f0ec
kwonsunbin <sunbin1696@gmail.com>
2021-04-14 09:25:12 +0900

    '14APR'

--- src/run.py
+++ src/run.py
@@ -4,10 +4,11 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
+from tqdm import tqdm
 
 dates_set = []
 
-for date in pd.date_range("2015-01-01","2021-04-01",freq="QS").tolist() :
+for date in pd.date_range("2015-01-01","2021-04-01",freq="A").tolist() :
     dates_set.append(('2010-01-01',str(date).split(" ")[0]))
 
 def input_days_and_span_selection() :
@@ -19,16 +20,17 @@ def input_days_and_span_selection() :
 
     result = []
 
-    for combination in input_days_span_combination :
+    for combination in tqdm(input_days_span_combination) :
         input_days = combination[0]
         span = combination[1]
         
         temp_result = []
         for date in dates_set :
+            
             start_date = date[0]
             end_date = date[1]
-            train(start_date, end_date, "sma", input_days, span, 15)
-            temp_result.append(test(start_date, end_date, "sma", input_days, span, 15))
+            train(start_date, end_date, "ema", input_days, span, 14)
+            temp_result.append(test(start_date, end_date, "ema", input_days, span, 14))
         result.append((input_days, span, temp_result))
 
     pickle.dump(result, open("inputdays&span", 'wb'))
@@ -39,7 +41,7 @@ def moving_average_selection() :
 
     result = {"sma": [], "wma" : [], "ema" : []}
     
-    for moving_average in available :
+    for moving_average in tqdm(available) :
         temp_result = []
         for date in dates_set :
             start_date = date[0]
@@ -53,13 +55,13 @@ def moving_average_selection() :
 
 def n_components_selection() :
     result = {}
-    for n in range(1,10):
+    for n in range(1,30):
         temp_result = []
         for date in dates_set :
             start_date = date[0]
             end_date = date[1]
-            train(start_date, end_date, "sma", 1, 3, n)
-            temp_result.append(test(start_date, end_date, "sma", 1, 3, n))
+            train(start_date, end_date, "ema", 1, 3, n)
+            temp_result.append(test(start_date, end_date, "ema", 1, 3, n))
 
         result[n] = temp_result
 
@@ -68,8 +70,8 @@ def n_components_selection() :
 
 
 def main():
-    #input_days_and_span_selection()
-    moving_average_selection()
+    input_days_and_span_selection()
+    #moving_average_selection()
     #n_components_selection()
 
 if __name__ == '__main__':
